406a3c726b7ccac8fe4fbb513c0ec09a85a6fc13
Avneet Singh <avneetsingh@avneets-mbp.lan>
2023-06-19 15:03:09 -0400

    fixed formatting

--- arraysCommunication.py
+++ arraysCommunication.py
@@ -1,64 +1,70 @@
-import datetime, math, time
-from datetime import datetime
+import datetime
+import math
+import time
 
-#VAR DECLARATION
 
-counter = 0
-logCounter = 10
+def manipulate_arrays():
+    # VAR DECLARATION
+    counter = 0
+    log_counter = 10
 
-timeStorageForReversion = [] #helps with reversing the milisecond function
+    time_storage_for_reversion = []  # helps with reversing the millisecond function
 
-#LIST 1 CONTAINS INFORMATION THAT WILL BE USED IN THE SUCCESSION --> I USED NUMBERS FROM THE FIB SEQUENCE FOR FUN
+    # information_list contains information that will be used in the succession
+    # using numbers from the Fibonacci sequence for fun
+    information_list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
 
-informationList = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181] 
+    print(f'\nHere is the contents of Array 1: \n{information_list}\n')
 
-print(f'\nHere is the contents of Array 1: \n{informationList}\n')
+    # operation_list contains the operations that will be done to the value of information_list
+    operation_list = []
 
-#LIST 2 CONTAINS THE OPERATIONS THAT WILL BE DONE TO THE VALUE OF LIST 1
+    for i in range(len(information_list)):
+        counter += 1
+        now = datetime.datetime.now()
+        operation_list.append(information_list[i] - counter**2 + int(now.strftime("%f")))
+        time_storage_for_reversion.append(now.strftime("%f"))
 
-operationList = []
+    print(f'\nArray 2 manipulates the contents from array by subtracting an incrementing counter and then '
+          f'adding the current milliseconds and here is the output:')
+    print(operation_list)
 
-for i in range(len(informationList)):
-    counter += 1
-    now = datetime.now()
-    operationList.append(int(informationList[i]) - int(math.pow(counter,2)) + int(now.strftime("%f")))
-    timeStorageForReversion.append(now.strftime("%f"))
+    # final_manipulation_list performs operations that unite the contents of information_list and operation_list and then manipulate it even further
+    final_manipulation_list = []
 
-print(f'\nArray 2 manipulates the contents from array by subtracting an incrementing counter and then adding the current miliseconds and here is the output:')
-print(operationList)
+    for i in range(len(information_list)):
+        log_counter = int(str(log_counter) + '0')
+        final_manipulation_list.append((operation_list[i] - information_list[i]) * math.log(log_counter))
 
-#LIST 3 PERFORMS OPERATIONS THAT UNITE THE CONTENTS OF LISTS 1 AND 2 AND THEN MANIPUTLATE IT EVEN FURTHER
+    print(f'\nArray 3 manipulates the contents from 1 and 2 by unionizing them and then adding a log_counter '
+          f'to it and here is the output:')
+    print(final_manipulation_list)
 
-finalManipulationList = []
+    # ----------------------------------------------------------------------------------------------------------- #
+    time.sleep(2)
 
-for i in range(len(informationList)):
-    logCounter = int(str(logCounter) + '0')
-    finalManipulationList.append((int(operationList[i]) - int(informationList[i])) * int(math.log(logCounter)))
+    # Next, we will revert the terms of list 3 back into its original elements
+    print('\n\nNow we will revert the list!\n\n')
 
-print(f'\nArray 3 manipulates the contents from 1 and 2 by unionizing them and then adding a logCounter to it and here is the output:')
-print(finalManipulationList)
+    reversion_list = []
 
-#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-time.sleep(2)
+    log_counter = 10
+    counter = 0
 
-#NEXT WE WILL REVERT THE TERMS OF LIST 3 BACK INTO ITS ORIGIONAL ELEMENTS
+    for i in range(len(information_list)):
+        counter += 1
+        log_counter = int(str(log_counter) + '0')
+        reversion_list.append(int((final_manipulation_list[i]) / math.log(log_counter) + information_list[i]
+                                  - int(time_storage_for_reversion[i]) + counter**2))
 
-print('\n\nNow we will revert the list!\n\n')
+    time.sleep(2)
+    print(f'\nReverted Array uncovers everything in Array 3 back to 1 by using the contents of ARRAY 1 2 and 3 ---> here it is:')
+    print(reversion_list)
+    print("\nHere is the time Storage for Reversion:")
+    print(time_storage_for_reversion)
+    print("\n")
 
-reversionList = []
 
-logCounter = 10
-counter = 0 
-
-for i in range(len(informationList)):
-    counter += 1
-    logCounter = int(str(logCounter) + '0')
-    reversionList.append(int((finalManipulationList[i]) / int(math.log(logCounter)) + int(informationList[i]) - int(timeStorageForReversion[i]) + int(math.pow(counter,2))))
-
-time.sleep(2)
-print(f'\nReverted Array uncoverts everything in Array 3 back to 1 by using the contents of ARRAY 1 2 and 3 ---> here it is:')
-print(reversionList) 
-print("\nHere is the time Storage for Reversion:")
-print(timeStorageForReversion)
-print("\n")
+if __name__ == '__main__':
+    manipulate_arrays()
 
